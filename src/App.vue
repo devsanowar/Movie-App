ef59c4e72966f332185d212f8dfc384b4ba0ca14
devsanowar <mdsanowarhossen.cse@gmail.com>
2026-03-31 13:31:15 +0600

    UI Design done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
+  <header>
+    <router-link :to="{ name: 'home' }">
+      <h2 class="logo">Movie <span>App</span></h2>
+    </router-link>
+  </header>
 
+  <main>
+    <router-view> </router-view>
+  </main>
 </template>
 
-<style scoped>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+
+  &::selection {
+    background: transparentize($color: #34d999, $amount: 0.5);
+  }
+}
+
+body {
+  background-color: #454545;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color:#1e293b;
+  padding: 10px 20px;
+  box-shadow: 0px 0px 6px rgba($color: #000000, $alpha: 0.1);
+  h2 {
+    color: #fff;
+    font-size: 28px;
+
+    span {
+      color: #34d999;
+    }
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,191 @@
 <script setup></script>
 
-<template></template>
+<template>
+    <div class="home">
+        <div class="featured-movie-card">
+            <router-link to="/movie/tt2016894">
+                <img src="/images/movie-img.png"
+                    alt="movie-img" class="featured-movie-image" />
+
+                <div class="movie-detail">
+                    <h2>Ek Tha Tiger</h2>
+                    <p>
+                        A RAW agent, Tiger, is sent to Dublin to observe an Indian scientist who is
+                        suspected of sharing nuclear secrets with the ISI. He meets and falls for his
+                        caretaker Zoya, a girl with a dark secret.
+                    </p>
+                </div>
+            </router-link>
+        </div>
+
+        <div class="search-box">
+            <form action="">
+                <div class="form-group">
+                    <input type="text" placeholder="Search here">
+
+                    <input type="submit" value="Search" />
+                </div>
+            </form>
+        </div>
+
+        <div class="movie-list">
+            <div class="movie">
+                <router-link to="/movie/tt2016894" class="movie-link">
+                    <div class="movie-image">
+                        <img src="https://m.media-amazon.com/images/M/MV5BZjUzOWEzZTMtNDQzYS00ZDI4LWI4NjctNzcyN2NkY2IwMTA4XkEyXkFqcGc@._V1_SX300.jpg"
+                            alt="movie-img" class="featured-movie-image" />
+                        <div class="movie-type">Action Movie,Drama</div>
+                    </div>
+
+                    <div class="movie-detail">
+                        <p class="movie-year">2010</p>
+                        <h2>Ek Tha Tiger</h2>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    .home {
+        .featured-movie-card {
+            position: relative;
+
+
+            .featured-movie-image {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                position: relative;
+                z-index: 0;
+            }
+
+            .movie-detail {
+                position: absolute;
+                right: 0;
+                left: 0;
+                bottom: 0;
+                background-color: rgba($color: #000000, $alpha: 0.6);
+                padding: 15px;
+                z-index: 1;
+                
+                h2 {
+                    color: #fff;
+                    font-size: 24px;
+                    margin-bottom: 10px;
+                }
+
+                p {
+                    color: #f5f5f5;
+                }
+            }
+
+        }
+
+        .search-box {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding: 20px;
+
+            .form-group input {
+                display: block;
+                appearance: none;
+                border: none;
+                background: none;
+                outline: none;
+
+                &[type="text"] {
+                    background-color: transparentize($color: #000000, $amount: 0.1);
+                    color: #fff;
+                    font-size: 16px;
+                    padding: 10px 15px;
+                    border-radius: 6px;
+                    margin-bottom: 10px;
+
+                    &::placeholder {
+                        color: #f3f3f3;
+                        font-size: 16px;
+                    }
+
+                    &:focus {
+                        box-shadow: 0px 0px 6px rgba($color: #34D999, $alpha: 0.1);
+                    }
+                }
+
+                &[type="submit"] {
+                    width: 100%;
+                    max-width: 300px;
+                    font-size: 14px;
+                    font-weight: 600;
+                    background-color: #34D999;
+                    padding: 10px 16px;
+                    border-radius: 6px;
+                    color: #fff;
+                    text-transform: uppercase;
+                    transition: 0.4s;
+
+                    &:active {
+                        background-color: #1a9968;
+                    }
+                }
+            }
+        }
+
+        .movie{
+          display: flex;
+          flex-wrap: wrap;
+          margin: 0 8px;
+
+          .movie-link{
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+
+            .movie-image{
+              position: relative;
+              display: block;
+
+              img{
+                display: block;
+                max-width: 100%;
+                height: 300px;
+                object-fit: cover;
+              }
+
+              .movie-type{
+                position: absolute;
+                padding: 8px 15px;
+                background-color: #34D999;
+                color: #fff;
+                bottom: 10px;
+                left: 0;
+                text-transform: capitalize;
+              }
+            }
+
+            .movie-detail{
+              background-color: #1e293b;
+              padding: 10px 15px;
+              flex: 1 1 100%;
+              border-radius: 0px 0px 8px 8px;
+
+                .movie-year{
+                  color: #aaa;
+                  font-size: 14px;
+                  margin-bottom: 10px;
+                }
+
+                h2{
+                  color: #fff;
+                  font-size: 18px;
+                  font-weight: 600;
+                }
+            }
+          }
+
+        }
+    }
+</style>
